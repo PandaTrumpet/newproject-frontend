81a63ef6cf7c6e4118bf0bb30c3f113897d60a75
PandaTrumpet <dsavontrumpet@gmail.com>
2024-06-14 18:57:02 +0300

    update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import MovieList from "./components/MovieList/MovieList";
+import UserList from "./components/UserList/UserList";
+
+function App() {
+  return (
+    <>
+      <MovieList />
+
+      <UserList />
+    </>
+  );
+}
+
+export default App;
